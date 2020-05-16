590773b7d20c6bf95d1e0e748677c81b945f4e35
eastarj <eastarj@gmail.com>
2020-05-17 07:49:26 +0900

    .

--- pref_annotation_generator/src/main/java/dev/eastar/pref/annotation/generator/AnnotationGenerator.kt
+++ pref_annotation_generator/src/main/java/dev/eastar/pref/annotation/generator/AnnotationGenerator.kt
@@ -1,23 +1,21 @@
 package dev.eastar.pref.annotation.generator
 
 import dev.eastar.pref.annotation.Pref
-import dev.eastar.pref.annotation.generator.AnnotationConst.Companion.KAPT_KOTLIN_GENERATED_OPTION_NAME
 import dev.eastar.pref.annotation.util.Log
 import java.io.File
 import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
-import javax.lang.model.element.Element
 import javax.lang.model.element.TypeElement
 
 @SupportedSourceVersion(SourceVersion.RELEASE_8) // to support Java 8
-@SupportedOptions(KAPT_KOTLIN_GENERATED_OPTION_NAME)
+@SupportedOptions("kapt.kotlin.generated")
 public class AnnotationGenerator : AbstractProcessor() {
     private lateinit var kaptKotlinGeneratedDir: String
 
     override fun init(processingEnvironment: ProcessingEnvironment?) {
         super.init(processingEnvironment)
         Log.processingEnvironment = processingEnv
-        kaptKotlinGeneratedDir = processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME]!!
+        kaptKotlinGeneratedDir = processingEnv.options["kapt.kotlin.generated"]!!
     }
 
     override fun getSupportedSourceVersion(): SourceVersion {
@@ -30,20 +28,23 @@ public class AnnotationGenerator : AbstractProcessor() {
 
     //https://github.com/square/kotlinpoet
     override fun process(set: MutableSet<out TypeElement>?, roundEnvironment: RoundEnvironment?): Boolean {
-        set?.firstOrNull() ?: return true
         roundEnvironment
             ?.getElementsAnnotatedWith(Pref::class.java)
             ?.forEach {
-                generatePrefClass(it)
+                val packageName = it.enclosingElement.toString()
+                val className = "GeneratedAnnotationGenerator"
+
+                val file = File("$kaptKotlinGeneratedDir/${packageName.replace('.', '/')}", "HelloAnnotationsProcessGenerated.kt")
+                file.parentFile.mkdirs()
+                val fileContent = """
+
+        package $packageName
+        class HelloAnnotationsProcessGenerated {
+             fun hello() = "Hello annotations process"
+        }
+    """.trimIndent()
+                file.writeText(fileContent)
             }
         return true
-    }
-
-    @OptIn(ExperimentalStdlibApi::class)
-    private fun generatePrefClass(roundEnvironment: Element) {
-        val file = File("$kaptKotlinGeneratedDir/${roundEnvironment.enclosingElement.toString().replace('.', '/')}", "${roundEnvironment.simpleName}$CLASS_TAIL.kt")
-        file.parentFile.mkdirs()
-        val fileContent = ClassBuilderPreferences(roundEnvironment).getContent()
-        file.writeText(fileContent)
     }
 }
