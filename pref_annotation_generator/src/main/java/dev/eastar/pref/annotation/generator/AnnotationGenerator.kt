2ec17f57c732395b2fe30ee0d97d4efec4061837
eastarj <eastarj@gmail.com>
2020-05-17 04:52:06 +0900

    create module

---
+++ pref_annotation_generator/src/main/java/dev/eastar/pref/annotation/generator/AnnotationGenerator.kt
@@ -0,0 +1,6 @@
+package dev.eastar.pref.annotation.generator
+
+@SupportedSourceVersion(SourceVersion.RELEASE_8) // to support Java 8
+@SupportedOptions(KAPT_KOTLIN_GENERATED_OPTION_NAME)
+public class AnnotationGenerator : AbstractProcessor() {
+}
